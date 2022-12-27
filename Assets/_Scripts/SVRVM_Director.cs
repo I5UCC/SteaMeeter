a73c7b4a7f7addffda46029e3589eb86446cb393
Isaak <43730681+I5UCC@users.noreply.github.com>
2022-12-27 20:57:26 +0100

    Initial Code Commit

---
+++ Assets/_Scripts/SVRVM_Director.cs
@@ -0,0 +1,166 @@
+﻿using System.Xml;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using Valve.VR;
+using VoiceMeeter;
+using Voicemeeter;
+
+public class SVRVM_Director : MonoBehaviour
+{
+    private readonly string DEFAULTXMLPATH = Path.GetFullPath("default.xml");
+    private readonly string VRXMLFILEPATH = Path.GetFullPath("vr.xml");
+    private readonly string MANIFESTLFILEPATH = Path.GetFullPath("app.vrmanifest");
+    private readonly float INCREMENTVALUE = 2;
+
+	public Unity_Overlay menuOverlay;
+
+	[Space(10)]
+
+	public Slider slider1 = null;
+    public Slider slider2 = null;
+    public Slider slider3 = null;
+
+    [Space(10)]
+
+    public Text sliderTitle1 = null;
+    public Text sliderTitle2 = null;
+    public Text sliderTitle3 = null;
+
+    [Space(10)]
+
+    public Text sliderValue1 = null;
+    public Text sliderValue2 = null;
+    public Text sliderValue3 = null;
+
+    void Start() 
+	{
+        Remote.Initialize(RunVoicemeeterParam.VoicemeeterPotato);
+        Debug.Log(DEFAULTXMLPATH);
+        Debug.Log(VRXMLFILEPATH);
+        Debug.Log(MANIFESTLFILEPATH);
+        Reset();
+    }
+
+	public void OnApplicationQuit()
+	{
+        if (File.Exists(DEFAULTXMLPATH))
+        {
+            Remote.Load(DEFAULTXMLPATH);
+        }
+	}
+
+    public void OnSteamVRConnect()
+	{
+        if (File.Exists(MANIFESTLFILEPATH))
+        {
+            OpenVR.Applications.AddApplicationManifest(MANIFESTLFILEPATH, false);
+        }
+    }
+
+	public void OnSteamVRDisconnect()
+	{
+		Debug.Log("Quitting!");
+		Application.Quit();
+	}
+
+    private string GetEscapedXMLString(string xmlpath)
+    {
+        string xmlstring = File.ReadAllText(xmlpath);
+        return xmlstring.Replace("&", "&amp;");
+    }
+
+    private void LoadVMXML(string xmlpath)
+    {
+        Remote.Load(xmlpath);
+
+        XmlDocument xml = new XmlDocument();
+        xml.LoadXml(GetEscapedXMLString(xmlpath));
+
+        XmlNodeList stripnodes = xml.DocumentElement.SelectNodes("VoiceMeeterParameters/Strip");
+        foreach (XmlElement item in stripnodes)
+        {
+            string dblevel = item.GetAttribute("dblevel");
+            if (dblevel != "")
+            {
+                switch (item.GetAttribute("index"))
+                {
+                    case "6":
+                        slider1.value = float.Parse(dblevel);
+                        break;
+                    case "7":
+                        slider2.value = float.Parse(dblevel);
+                        break;
+                    case "8":
+                        slider3.value = float.Parse(dblevel);
+                        break;
+                }
+            }
+        }
+
+        string slidertext1 = xml.DocumentElement.SelectSingleNode("VoiceMeeterParameters/LabelVirtualStrip1").InnerText;
+        sliderTitle1.text = slidertext1 != "" ? slidertext1 : sliderTitle1.text;
+        string slidertext2 = xml.DocumentElement.SelectSingleNode("VoiceMeeterParameters/LabelVirtualStrip2").InnerText;
+        sliderTitle2.text = slidertext2 != "" ? slidertext2 : sliderTitle2.text;
+        string slidertext3 = xml.DocumentElement.SelectSingleNode("VoiceMeeterParameters/LabelVirtualStrip3").InnerText;
+        sliderTitle3.text = slidertext3 != "" ? slidertext3 : sliderTitle3.text;
+    }
+
+    private void SetSliders()
+    {
+        string slidertext1 = Remote.GetTextParameter("Strip[5].Label");
+        sliderTitle1.text = slidertext1 != "" ? slidertext1 : sliderTitle1.text;
+        string slidertext2 = Remote.GetTextParameter("Strip[6].Label");
+        sliderTitle2.text = slidertext2 != "" ? slidertext2 : sliderTitle2.text;
+        string slidertext3 = Remote.GetTextParameter("Strip[7].Label");
+        sliderTitle3.text = slidertext3 != "" ? slidertext3 : sliderTitle3.text;
+
+        slider1.value = Remote.GetParameter("Strip[5].Gain");
+        slider2.value = Remote.GetParameter("Strip[6].Gain");
+        slider3.value = Remote.GetParameter("Strip[7].Gain");
+    }
+
+    public void Reset()
+    {
+        if (File.Exists(VRXMLFILEPATH))
+        {
+            LoadVMXML(VRXMLFILEPATH);
+        }
+        else
+        {
+            SetSliders();
+        }
+    }
+    
+    public void Restart() => Remote.Restart();
+
+    public void SetOutput1Volume(float value)
+    {
+        Remote.SetParameter("Strip[5].Gain", value);
+        sliderValue1.text = value.ToString("n1");
+    }
+
+    public void SetOutput2Volume(float value)
+    {
+        Remote.SetParameter("Strip[6].Gain", value);
+        sliderValue2.text = value.ToString("n1");
+    }
+
+    public void SetOutput3Volume(float value)
+    {
+        Remote.SetParameter("Strip[7].Gain", value);
+        sliderValue3.text = value.ToString("n1");
+    }
+
+    public void IncrementOutput1() => slider1.value += INCREMENTVALUE;
+
+    public void IncrementOutput2() => slider2.value += INCREMENTVALUE;
+
+    public void IncrementOutput3() => slider3.value += INCREMENTVALUE;
+
+    public void DecrementOutput1() => slider1.value -= INCREMENTVALUE;
+
+    public void DecrementOutput2() => slider2.value -= INCREMENTVALUE;
+
+    public void DecrementOutput3() => slider3.value -= INCREMENTVALUE;
+}
---
+++ Assets/_Scripts/WindowController.cs
@@ -0,0 +1,228 @@
+﻿/*
+MIT License
+
+Copyright (c) 2017 Ben Otter
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. 
+*/
+
+using System;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+using UnityEngine;
+
+public class WindowController : MonoBehaviour
+{
+    public bool hasTrayIcon = true;
+    public Texture2D trayIconTex;
+
+    private const int GWL_EXSTYLE = -0x14;
+    private const int WS_EX_TOOLWINDOW = 0x0080;
+    private const int SWP_HIDEWINDOW = 0x0080;
+
+    private IntPtr winHandle;
+
+    private SVRVMTrayForm trayForm;
+
+
+#if UNITY_STANDALONE_WIN && !UNITY_EDITOR
+
+    [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)] static extern IntPtr FindWindowByCaption(IntPtr zeroOnly, string lpWindowName);
+    [DllImport("user32.dll", EntryPoint = "SetWindowPos")] private static extern bool SetWindowPos(IntPtr hwnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+    [DllImport("user32.dll")] [return: MarshalAs(UnmanagedType.Bool)] private static extern bool ShowWindow(IntPtr hWnd, uint nCmdShow);
+    [DllImport("user32.dll")] static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+    [DllImport("user32.dll", SetLastError = true)] static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+#else
+
+    static IntPtr FindWindowByCaption(IntPtr zeroOnly, string lpWindowName) { return IntPtr.Zero; }
+    static int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong) { return 0; }
+    static int GetWindowLong(IntPtr hWnd, int nIndex) { return 0; }
+    private static bool SetWindowPos(IntPtr hwnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags) { return true; }
+    private static bool ShowWindow(IntPtr hWnd, uint nCmdShow) { return true; }
+
+#endif
+    void Start()
+    {
+        winHandle = FindWindowByCaption(IntPtr.Zero, UnityEngine.Application.productName);
+
+        if(hasTrayIcon)
+            CreateTray();
+
+        HideUnityWindow();
+        ShowTrayIcon();
+    }
+
+    void OnEnable()
+    {
+        if(hasTrayIcon)
+            CreateTray();
+    }
+
+    void OnDestroy() 
+    {
+        DestroyTray();
+    }
+
+    void OnApplicationQuit()
+    {
+        DestroyTray();
+    }
+
+    void OnDisable()
+    {
+        DestroyTray();
+    }
+
+    public void CreateTray()
+    {
+#if !UNITY_EDITOR
+        if(trayForm == null)
+        {
+            trayForm = new SVRVMTrayForm(trayIconTex); //CreateIcon(trayIconTex));
+
+            trayForm.onExitCallback += OnExit;
+        }
+#endif
+    }
+
+    public void DestroyTray()
+    {
+        if(trayForm != null)
+        {
+            trayForm.Dispose();
+            trayForm = null;
+        }
+    }
+
+    public void OnExit() 
+    {
+        UnityEngine.Application.Quit();
+    }
+
+    public void ShowTrayIcon()
+    {
+        if(trayForm != null)
+            trayForm.ShowTray();
+    }
+
+    public bool HideTaskbarIcon() 
+    {
+        bool res = false;
+
+        res = ShowWindow(winHandle, (uint)0);
+        SetWindowLong(winHandle, GWL_EXSTYLE, GetWindowLong(winHandle, GWL_EXSTYLE) | WS_EX_TOOLWINDOW);
+        res = ShowWindow(winHandle, (uint)5);
+
+        return res;
+    }
+
+    public bool MinimizeUnityWindow()
+    {
+        bool res = ShowWindow(winHandle, (uint) 6);
+        return res;
+    }
+
+    public bool HideUnityWindow()
+    {
+        bool res = false;
+        
+        res = MinimizeUnityWindow();
+        res = HideTaskbarIcon();
+
+        return res;
+    }
+}
+
+public class SVRVMTrayForm : System.Windows.Forms.Form 
+{
+    public delegate void OnExitDel();
+    public delegate void OnShowWindowDel();
+
+    public OnExitDel onExitCallback;
+    public OnShowWindowDel onShowWindow;
+
+    private System.Windows.Forms.ContextMenuStrip trayMenu;
+    private NotifyIcon trayIcon;
+
+    public SVRVMTrayForm(Texture2D tex = null)
+    {
+        trayMenu = new System.Windows.Forms.ContextMenuStrip();
+
+        trayMenu.Items.Add("Exit", CreateBitmap(Texture2D.whiteTexture), OnExit);
+
+        trayIcon = new NotifyIcon();
+        trayIcon.Text = "SVRVM";
+
+        trayIcon.ContextMenuStrip = trayMenu;
+
+        if(tex == null)
+            trayIcon.Icon = new Icon(SystemIcons.Application, 40, 40);
+        else
+            trayIcon.Icon = Icon.FromHandle(CreateBitmap(tex).GetHicon());
+    }
+
+    public Bitmap CreateBitmap(Texture2D tex)
+    {
+        MemoryStream memS = new MemoryStream(tex.EncodeToPNG());
+        memS.Seek(0, System.IO.SeekOrigin.Begin);
+
+        Bitmap bitmap = new Bitmap(memS);
+        
+        return bitmap;
+    }
+
+    protected override void OnLoad(EventArgs e)
+    {
+        Visible = false;
+        ShowInTaskbar = false;
+
+        base.OnLoad(e);
+    }
+
+    ~SVRVMTrayForm()
+    {
+        Dispose(true);
+    }
+
+    public void ShowTray()
+    {
+        trayIcon.Visible = true;
+    }
+
+    public void HideTray()
+    {
+        trayIcon.Visible = false;
+    }
+
+    protected void OnExit(object sender, EventArgs e)
+    {
+        onExitCallback.Invoke();
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if(disposing)
+            trayIcon.Dispose();
+
+        base.Dispose(disposing);
+    }
+}
